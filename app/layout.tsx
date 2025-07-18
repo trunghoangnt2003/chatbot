d45a5809deff9f07e0cb77ee627d86d38ca696a2
trung <trungnt260303@gmail.com>
2025-07-18 14:29:01 +0700

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://www.facebook.com/trungnt2603?locale=vi_VN'),
+  title: 'TrungNT2603 Chatbot',
+  description: 'TrungNT2603 Chatbot using the AI SDK.',
 };
 
 export const viewport = {
